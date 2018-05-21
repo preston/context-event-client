0db49c028006ca608a4757fc0c3b2ed353aee710
Preston Lee <preston.lee@prestonlee.com>
2018-05-21 11:24:38 -0700

    Trying to fix exports.

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import { BaseEvent } from "./lib/context_event/base_event";
-import {ActionEvent} from "./lib/context_event/action_event";
-import {HintEvent} from "./lib/context_event/hint_event";
-import {ContextEventService} from "./lib/context_event/service";
-import {ContextEventSession} from "./lib/context_event/session";
+export { BaseEvent } from "./lib/context_event/base_event";
+export {ActionEvent} from "./lib/context_event/action_event";
+export {HintEvent} from "./lib/context_event/hint_event";
+export {ContextEventService} from "./lib/context_event/service";
+export {ContextEventSession} from "./lib/context_event/session";
