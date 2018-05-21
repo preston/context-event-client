0cb6e25ef5c8ed923d9cbc7f9fc9fe4054d3026f
Preston Lee <preston.lee@prestonlee.com>
2018-05-21 11:19:32 -0700

    Converting to NPM package.

---
+++ index.ts
@@ -0,0 +1,5 @@
+import { BaseEvent } from "./lib/context_event/base_event";
+import {ActionEvent} from "./lib/context_event/action_event";
+import {HintEvent} from "./lib/context_event/hint_event";
+import {ContextEventService} from "./lib/context_event/service";
+import {ContextEventSession} from "./lib/context_event/session";
