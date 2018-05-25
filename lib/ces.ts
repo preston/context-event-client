67802d6c0ff29a1ddef534d8f23e83a08e22a57a
frostbytedata <contact@frostbytedata.com>
2018-05-25 16:27:38 -0400

    Adding JWT logic to event service. TODO: handle expirey

---
+++ lib/ces.ts
@@ -0,0 +1,70 @@
+export abstract class EventModel {
+  person_id: string;
+  topic_uri: string;
+  model_uri: string;
+  agent_uri: string;
+  parameters: any;
+}
+
+export abstract class JWTResponse {
+  jwt: string;
+  authorization: string;
+}
+class CES {
+  url: string = 'http://context-event-service.prestonlee.com';
+  token: JWTResponse;
+  constructor(){
+
+  }
+  send (event: EventModel) {
+    return new Promise((resolve, reject) => {
+      if(!this.token){
+        this.getJWT().then((token: JWTResponse) => {
+          this.sendRequest(event, token).then((response) => {
+            resolve(response);
+          });
+        });
+      } else {
+        this.sendRequest(event, this.token).then((response) => {
+          resolve(response);
+        });
+      }
+    });
+  }
+  sendRequest(event, token){
+    return new Promise((resolve, reject) => {
+      fetch(this.url + '/events', {
+        method: 'POST',
+        body: JSON.stringify(event),
+        headers:{
+          'Content-Type': 'application/json',
+          'Authorization': token.authorization
+        }
+      }).then((response) => {
+        resolve(response);
+      }).catch((error) => {
+        reject(error);
+      });
+    });
+  };
+  getJWT(){
+    return new Promise((resolve, reject) => {
+      fetch(this.url + '/sessions', {
+        method: 'POST',
+        body: JSON.stringify(event),
+        headers:{
+          'Content-Type': 'application/json'
+        }
+      }).then((response: Response) => {
+        response.json().then((data: JWTResponse) => {
+          this.token = data;
+          resolve(data);
+        });
+      }).catch((error) => {
+        reject(error);
+      });
+    });
+  }
+}
+
+export default CES;
