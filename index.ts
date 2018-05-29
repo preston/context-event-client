61168171835578532986f2e18b34bea460bec5dc
frostbytedata <contact@frostbytedata.com>
2018-05-29 15:39:26 -0400

    Tweaking exports. Increasing version number

--- index.ts
+++ index.ts
@@ -1,10 +1,6 @@
-import {JWTResponse} from "./lib/ces";
-
 export { BaseEvent } from "./lib/context_event/base_event";
-export {ActionEvent} from "./lib/context_event/action_event";
-export {HintEvent} from "./lib/context_event/hint_event";
-export {ContextEventService} from "./lib/context_event/service";
-export {ContextEventSession} from "./lib/context_event/session";
-export { EventModel } from "./lib/ces";
-export { JWTResponse } from "./lib/ces";
-export { CES } from "./lib/ces";
+export { ActionEvent } from "./lib/context_event/action_event";
+export { HintEvent } from "./lib/context_event/hint_event";
+export { ContextEventService } from "./lib/context_event/service";
+export { ContextEventSession } from "./lib/context_event/session";
+export { CES, EventModel, JWTResponse } from "./lib/ces";
