82c2586509aaec61968f904ed8f483ce78d9c95d
frostbytedata <contact@frostbytedata.com>
2018-05-30 17:04:41 -0400

    Enable 'new'ing for ActionEvent objects so we can set some constants from the class definition

--- lib/context_event/action_event.ts
+++ lib/context_event/action_event.ts
@@ -7,5 +7,13 @@ export class ActionEvent {
 	public controller_uri: string = '';
 	public model_uri: string = '';
 	public parameters: any = '';
-
+	constructor(topic_uri: string,
+				controller_uri: string,
+				model_uri: string,
+				parameters: any){
+		this.topic_uri = topic_uri;
+		this.controller_uri = controller_uri;
+		this.model_uri = model_uri;
+		this.parameters = parameters;
+	}
 }
