3822fe44dd7f6134bcc5ff6d0a85a11f333da0fb
frostbytedata <contact@frostbytedata.com>
2018-05-31 12:13:51 -0400

    Provide Ontology URI prefix so clients don't need to keep track of it.

--- lib/context_event/action_event.ts
+++ lib/context_event/action_event.ts
@@ -1,6 +1,7 @@
 export class ActionEvent {
 
 	public static EVENT_TYPE_ACTION: string = 'artaka://events/action';
+	public static EVENT_TOPIC_PREFIX: string = 'http://www.ke.tu-darmstadt.de/ontologies/ui_detail_level.owl#';
 
 	public type: string = ActionEvent.EVENT_TYPE_ACTION;
 	public topic_uri: string = '';
@@ -11,7 +12,7 @@ export class ActionEvent {
 				controller_uri: string,
 				model_uri: string,
 				parameters: any){
-		this.topic_uri = topic_uri;
+		this.topic_uri = ActionEvent.EVENT_TOPIC_PREFIX + topic_uri;
 		this.controller_uri = controller_uri;
 		this.model_uri = model_uri;
 		this.parameters = parameters;
